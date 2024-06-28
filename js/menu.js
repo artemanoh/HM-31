33c14c78072aedee1aaab74b74bdf2d0f8ee7c9a
artem1214 <61391756+artemanoh@users.noreply.github.com>
2024-06-28 19:28:07 +0300

    Зробив меню

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+
+  (() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-backdrop-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-backdrop-menu-close]"),
+      menu: document.querySelector("[data-backdrop-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
